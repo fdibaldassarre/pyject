b3dbe695ee0270ad88874d623e5ba9df77f02546
Francesco Di Baldassarre <f.dibaldassarre@gmail.com>
2024-01-17 23:29:23 +0100

    Fix singleton created multiple times

--- pyject/__init__.py
+++ pyject/__init__.py
@@ -1,4 +1,5 @@
-from typing import TypeVar, Type, Any
+from logging import getLogger
+from typing import TypeVar, Type, Any, Optional
 
 from pyject.common import Scope, AbstractModule, DependencyIdentifier
 from pyject.mapper import MappingStore, DependencyInfo
@@ -13,6 +14,7 @@ class Injector:
     def __init__(self, mapping_store: MappingStore):
         self.mapping_store = mapping_store
         self.services = dict()
+        self.logging = getLogger(Injector.__name__)
 
     def _create(self, instance_info: DependencyInfo) -> T:
         instance_class = instance_info.instance_class
@@ -22,18 +24,19 @@ class Injector:
         for key, requirement in args.items():
             if key not in arguments:
                 arguments[key] = self._get_instance(requirement)
+        self.logging.debug(f"Creating {instance_info.instance_class} [{instance_info.instance_name}]")
         return instance_class(**arguments) if instance_constructor is None else instance_constructor(**arguments)
 
     def _get_instance(self, ide: DependencyIdentifier) -> Any:
         instance_info = self.mapping_store.get_actual_instance_with_scope(ide)
         if instance_info.scope == Scope.NO_SCOPE:
             return self._create(instance_info)
-        if instance_info.instance_class not in self.services:
+        if ide not in self.services:
             self.services[ide] = self._create(instance_info)
         return self.services[ide]
 
-    def get_instance(self, instance_class: Type) -> S:
-        return self._get_instance(DependencyIdentifier(instance_class))
+    def get_instance(self, instance_class: Type[S], instance_name: Optional[str] = None) -> S:
+        return self._get_instance(DependencyIdentifier(instance_class, instance_name))
 
 
 class PyJect:
